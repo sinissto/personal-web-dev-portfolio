cb5aa9d2f3817c81dcb39011ec80085705b40499
Sinisa <sinisha.stojanovic@gmail.com>
2025-07-08 21:40:56 +0200

    Rough app markup structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <p>Web Developer Portfolio</p>
+    <div className={"o-page__inner o-page__inner--home"}>
+      <section className={"o-section c-homepage-slider"}>
+        <div className={"c-homepage-slider__inner"}>
+          <div className="o-container"> SWIPER CONTAINER </div>
+        </div>
+
+        <div className="c-homepage-slider__canvas">
+          CANVAS WITH VIDEO ELEMENT THAT ROTATE LIKE CAROUSEL
+        </div>
+
+        <div className="c-homepage-slider__pagination">SIDE NAV</div>
+
+        <i className={"c-scroll-indicator"}>SCROLL INDICATOR</i>
+      </section>
     </div>
   );
 }

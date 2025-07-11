c8c087663a2adac08ca9e089d128c81f80d79c83
Sinisa <sinisha.stojanovic@gmail.com>
2025-07-11 22:32:34 +0200

    NavBar component markup finished and included in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,30 @@ import "./App.css";
 import Slider from "./components/Slider.jsx";
 import SideNav from "./components/SideNav.jsx";
 import ScrollIndicator from "./components/ScrollIndicator.jsx";
+import NavBar from "./components/navigation/NavBar.jsx";
 
 function App() {
   return (
-    <div className={"o-page__inner o-page__inner--home"}>
-      <section className={"o-section c-homepage-slider"}>
-        <div className={"c-homepage-slider__inner"}>
-          <div className="o-container">
-            <Slider />
+    <>
+      <NavBar />
+      <div className={"o-page__inner o-page__inner--home"}>
+        <section className={"o-section c-homepage-slider"}>
+          <div className={"c-homepage-slider__inner"}>
+            <div className="o-container">
+              <Slider />
+            </div>
           </div>
-        </div>
 
-        {/*<div className="c-homepage-slider__canvas">*/}
-        {/*  CANVAS WITH VIDEO ELEMENT THAT ROTATE LIKE CAROUSEL*/}
-        {/*</div>*/}
+          {/*<div className="c-homepage-slider__canvas">*/}
+          {/*  CANVAS WITH VIDEO ELEMENT THAT ROTATE LIKE CAROUSEL*/}
+          {/*</div>*/}
 
-        <SideNav />
+          <SideNav />
 
-        <ScrollIndicator />
-      </section>
-    </div>
+          <ScrollIndicator />
+        </section>
+      </div>
+    </>
   );
 }
 

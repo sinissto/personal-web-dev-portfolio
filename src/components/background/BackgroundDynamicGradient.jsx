489c6efb41784b468b6a75a11202724cf82f56ab
Sinisa <sinisha.stojanovic@gmail.com>
2025-07-22 22:35:19 +0200

    Modified BackgroundDynamicGradient component in that way I initialized gradiand object from Gradient class

--- src/components/background/BackgroundDynamicGradient.jsx
+++ src/components/background/BackgroundDynamicGradient.jsx
@@ -1,19 +1,23 @@
 import "./BackgroundDynamicGradient.css";
-import gradient from "../../scripts/Gradient.js";
+import Gradient from "../../scripts/Gradient.js";
 import { useEffect } from "react";
+
+const gradient = new Gradient();
 
 const BackgroundDynamicGradient = () => {
   useEffect(() => {
     gradient.initGradient("#gradient-canvas");
+    console.log(gradient);
   }, []);
 
   return (
     <div className="c-page-bg js-page-bg js-gradient-bg is-loaded c-gradient-bg">
-      {/*<i className="c-page-bg__bg js-page-bg-bg"></i>*/}
-      <canvas
-        id={"gradient-canvas"}
-        className="is-loaded c-gradient-bg"
-      ></canvas>
+      <i className="c-page-bg__bg js-page-bg-bg">
+        <canvas
+          id={"gradient-canvas"}
+          className="is-loaded c-gradient-bg"
+        ></canvas>
+      </i>
     </div>
   );
 };
